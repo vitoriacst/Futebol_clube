70937f3b04a171854e4f71b8d4b5d6a1cddb2d3a
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-18 16:14:21 -0300

    perf: :wrench: lifting docker containers

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -4,6 +4,7 @@ class LoginService {
   static async login(email: string, password: string) {
     const sucessCase = await modelUsers.findOne({ where: { email, password } });
     // vai procurar o email e senha
+    // teste
     return sucessCase;
   }
 }
