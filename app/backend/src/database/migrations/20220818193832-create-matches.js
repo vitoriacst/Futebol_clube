089cd58c5dfe3d8d443494daeed4da8c6e34f753
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-18 17:01:37 -0300

    feat: :sparkles: implementing the matches migration

--- app/backend/src/database/migrations/20220818193832-create-matches.js
+++ app/backend/src/database/migrations/20220818193832-create-matches.js
@@ -2,20 +2,55 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('matches' , {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "home_team",
+        references: {
+          model: "teams",
+          key: "id",
+          // faz a referencia a chave estrangeira id
+        },
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "home_team_goals",
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "away_team",
+        references: {
+          model: "teams",
+          key: "id",
+        },
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "away_team_goals",
+      },
+      inProgress: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: "in_progress",
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable("matches");
+  },
 };
+
+
+
+  
