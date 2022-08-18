4a74b14e377b794abd98f5d97302d9535d3bd243
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-18 16:29:57 -0300

    feat: :sparkles: implementing migration teams

--- app/backend/src/database/migrations/20220818192446-create-.js
+++ app/backend/src/database/migrations/20220818192446-create-.js
@@ -1,21 +1,23 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'team_name'
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+    await queryInterface.dropTable('teams');
+  },
+}
