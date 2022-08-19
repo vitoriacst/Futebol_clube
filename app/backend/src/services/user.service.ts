b9d44c0875125700f7fb4eaf962e996ef106649a
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 16:56:45 -0300

    feat: :sparkles: rescuing the user's email

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,5 +1,13 @@
-import { IUserService } from '../interfaces/ILogin';
-// -|> implementando a interface IUserService , todos os metosos que teram na interface IuserService tera que ter na classe
-export default class UserService implements IUserService {
+import modelUsers from '../database/models/users.model';
+import { IUser, IUserService } from '../interfaces/ILogin';
 
+// -|> implementando a interface IUserService , todos os metodos que ira ter interface IuserService tera que ter na classe
+
+export default class UserService implements IUserService {
+  static async findEMail(email:string):Promise<IUser | null > {
+    const emailUser: IUser | null = await modelUsers.findOne({
+      where: { email },
+    });
+    return emailUser;
+  }
 }
