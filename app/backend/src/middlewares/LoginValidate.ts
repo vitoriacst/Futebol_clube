796370e7fff9f971822bfbeac33512b18174be30
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 16:08:40 -0300

    perf: :fire: changing the form of validation, starting with JOI

--- app/backend/src/middlewares/LoginValidate.ts
+++ app/backend/src/middlewares/LoginValidate.ts
@@ -1,20 +1,16 @@
-import { NextFunction, Request, Response } from 'express';
-import { ICredentials } from '../interfaces/ILogin';
-
 export default class LoginValidate {
-  public validate =
-  async (req: Request, res:Response, next: NextFunction): Promise<(Response | void)> => {
-    const { email, password } = req.body as ICredentials;
+  // public validate =
+  // async (req: Request, res:Response, next: NextFunction): Promise<(Response | void)> => {
+  //   const { email, password } = req.body as ICredentials;
 
-    if (!email || email === '') {
-      return res.status(400).json({ message: 'All fields must be filled' });
-    }
-    if (!password || password === '') {
-      return res.status(400).json({ message: 'All fields must be filled' });
-    }
+  //   if (!email || email === '') {
+  //     return res.status(400).json({ message: 'All fields must be filled' });
+  //   }
+  //   if (!password || password === '') {
+  //     return res.status(400).json({ message: 'All fields must be filled' });
+  //   }
 
-    return next();
-  };
+  //   return next();
 }
 
 // -|> realizando a validacao por meio da interface de credenciais criada
