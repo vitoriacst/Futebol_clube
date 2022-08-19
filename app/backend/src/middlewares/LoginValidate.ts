7bd6a763d4acf4351a9d4deffb1d7a1f50f7697f
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 16:15:04 -0300

    perf: :construction: starting to use joi for validation

--- app/backend/src/middlewares/LoginValidate.ts
+++ app/backend/src/middlewares/LoginValidate.ts
@@ -1,8 +1,14 @@
+import Joi = require('joi');
 import { ICredentials } from '../interfaces/ILogin';
 
 export default class LoginValidate {
   static validateLogin = async (credentials : ICredentials): Promise<ICredentials> => {
-
+    const message = 'All fields must be filled';
+    const validate = Joi.object({
+      email: Joi.string().email().required().messages({
+        'string.empty': message,
+      }),
+    });
   };
 }
 
