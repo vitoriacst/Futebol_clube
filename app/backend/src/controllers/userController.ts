3fbdd663ede6d8c116cc004119f16820f0a29f99
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 17:36:28 -0300

    feat: :sparkles: implementing the UserLoginValidate method and ending the users controller

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -1,5 +1,14 @@
+import { Request, Response } from 'express';
 import { IUserService } from '../interfaces/ILogin';
+import LoginValidate from '../middlewares/LoginValidate';
 
 export default class UserController {
   constructor(private _userService: IUserService) {}
+  async UserLoginValidate(res: Response, req: Request): Promise<void> {
+    await LoginValidate.validateLogin(req.body);
+    // puxando do middleware de validacao
+    const response = await this._userService.login(req.body);
+    // validando a existencia do token e retornando o status 200, caso o login seja efetuado com sucesso;
+    res.status(200).json({ token: response?.token });
+  }
 }
