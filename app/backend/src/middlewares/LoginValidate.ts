f9baeea6cb7aedbc0150b6cdbfa900ecf47dc230
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 16:11:27 -0300

    perf: :construction: implementing the LoginValidate class

--- app/backend/src/middlewares/LoginValidate.ts
+++ app/backend/src/middlewares/LoginValidate.ts
@@ -1,16 +1,9 @@
+import { ICredentials } from 'src/interfaces/ILogin';
+
 export default class LoginValidate {
-  // public validate =
-  // async (req: Request, res:Response, next: NextFunction): Promise<(Response | void)> => {
-  //   const { email, password } = req.body as ICredentials;
+  static validateLogin = async (credentials : ICredentials): Promise<ICredentials> => {
 
-  //   if (!email || email === '') {
-  //     return res.status(400).json({ message: 'All fields must be filled' });
-  //   }
-  //   if (!password || password === '') {
-  //     return res.status(400).json({ message: 'All fields must be filled' });
-  //   }
-
-  //   return next();
+  };
 }
 
 // -|> realizando a validacao por meio da interface de credenciais criada
