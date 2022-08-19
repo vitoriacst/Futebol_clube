79513446165bca07717b0b0adaecad18afe361e8
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 16:22:27 -0300

    feat: :sparkles: creating Errors middleware for error typing

--- app/backend/src/middlewares/LoginValidate.ts
+++ app/backend/src/middlewares/LoginValidate.ts
@@ -4,12 +4,15 @@ import { ICredentials } from '../interfaces/ILogin';
 export default class LoginValidate {
   static validateLogin = async (credentials : ICredentials): Promise<ICredentials> => {
     const message = 'All fields must be filled';
-    const validate = Joi.object({
+    const usersInformations = Joi.object({
       email: Joi.string().email().required().messages({
         'string.empty': message,
       }),
       password: Joi.string().min(6).required(),
     });
+    const { error } = usersInformations.validate(credentials);
+    if (error?.message.includes('must be filled')) {
+    }
   };
 }
 
