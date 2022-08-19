f2436d52191acbc388540aa4d3e3b3aebc013fb2
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-19 16:52:36 -0300

    feat: :construction: creating the UserService class that implements the IUserService interface

---
+++ app/backend/src/services/user.service.ts
@@ -0,0 +1,5 @@
+import { IUserService } from '../interfaces/ILogin';
+// -|> implementando a interface IUserService , todos os metosos que teram na interface IuserService tera que ter na classe
+export default class UserService implements IUserService {
+
+}
