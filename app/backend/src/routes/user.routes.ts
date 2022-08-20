907e34003dc71779feda5282fe8da279d7cf973c
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-20 19:02:11 -0300

    refactor: :ambulance: tried to refactor the routes

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 import UserController from '../controllers/userController';
 
 const user:Router = Router();
-user.post('/login', (req:Request, res: Response) => UserController.login(req, res));
+user.post('/login', UserController.login);
 user.get('/users', (req:Request, res: Response) => UserController.getAllUsers(req, res));
 
 export default user;
