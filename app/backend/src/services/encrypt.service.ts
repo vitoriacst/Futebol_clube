24fd27d473ccc0b49a802a04f0fe6f1c25b57c6d
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-22 18:31:31 -0300

    feat: :sparkles: implementing password encryption using the bcrypt library

--- app/backend/src/services/encrypt.service.ts
+++ app/backend/src/services/encrypt.service.ts
@@ -1,5 +1,8 @@
 import * as bcrypt from 'bcryptjs';
 
 export default class EncryptService {
-  
+  static validatePassword = (password: string, hash: string): boolean =>
+    bcrypt.compareSync(password, hash);
 }
+
+// -|> esconder senhas criadas pelos usuários em forma de texto “puro” em dados indecifráveis, utilizando o algoritmo hash
