5ba53a5f8d20cc9087e9b2a67afd97bb70ce6a34
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-22 19:34:11 -0300

    fix: :monocle_face: testing the login route break reason

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -5,6 +5,6 @@ const user:Router = Router();
 
 user.post('/login', (req:Request, res: Response) => UserController.login(req, res));
 user.get('/users', (req:Request, res: Response) => UserController.getAllUsers(req, res));
-user.get('/validate', (request, response) => UserController.validateUser(request, response));
+// user.get('/validate', (request, response) => UserController.validateUser(request, response));
 
 export default user;
