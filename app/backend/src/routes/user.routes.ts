7b9d38ae5026876c8fcb65f1e4108cd70aae6bbe
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-22 19:12:55 -0300

    style: :art: code formatting

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import UserController from '../controllers/userController';
 
 const user:Router = Router();
+
 user.post('/login', (req:Request, res: Response) => UserController.login(req, res));
 user.get('/users', (req:Request, res: Response) => UserController.getAllUsers(req, res));
 user.get('/validate', (request, response) => UserController.validateUser(request, response));
