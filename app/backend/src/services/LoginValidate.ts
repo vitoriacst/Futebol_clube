767740de31829b60956ed84940d9aa32c457e52b
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-22 18:15:03 -0300

    feat: :sparkles: implementing validation with joi

--- app/backend/src/services/LoginValidate.ts
+++ app/backend/src/services/LoginValidate.ts
@@ -1,25 +1,27 @@
 import * as Joi from 'joi';
+import { ICredentials } from '../interfaces/ILogin';
 import Errors from '../middlewares/Errors';
 
-export default class LoginValidate {
-  static validateLogin = async (credentials: any) => {
-    const message = 'All fields must be filled';
-    const usersInformations = Joi.object({
-      email: Joi.string().email().required().messages({
-        'string.empty': message,
-      }),
+export default class JoiService {
+  static validateLogin = async (credentials: ICredentials): Promise<ICredentials> => {
+    const userInformation = Joi.object({
+      email: Joi.string().email().required(),
       password: Joi.string().min(6).required(),
     });
-    const { error } = usersInformations.validate(credentials);
-    if (error?.message.includes('must be filled')) {
-      throw new Errors(401, error.message);
-      // -> Erros vem diretamente do MiddleWare de erro
+    const { error } = userInformation.validate(credentials);
+    if (error?.message.includes('empty')) {
+      throw new Errors(400, 'All fields must be filled');
     }
+    if (error?.message.includes('required')) {
+      throw new Errors(400, 'All fields must be filled');
+    }
+    if (error?.message.includes('must be filled')) {
+      throw new Errors(400, error.message);
+    }
+    // tratando o tamanho dos campos inseridos
     if (error?.message.includes('length')) {
       throw new Errors(401, 'Incorrect email or password');
     }
     return credentials;
   };
 }
-
-// -|> realizando a validacao por meio da interface de credenciais criada
