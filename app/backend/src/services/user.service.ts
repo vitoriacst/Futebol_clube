22a8a74b2cb33b0a60885ce672996a12ad3bebdc
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-22 18:34:42 -0300

    feat: :sparkles: implementing password existence validation in the service layer

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,7 +1,8 @@
-import Errors from '../middlewares/Errors';
 import modelUsers from '../database/models/users.model';
-import JwtService from './Jwt.service';
 import { IUser } from '../interfaces/ILogin';
+import Errors from '../middlewares/Errors';
+import EncryptService from './encrypt.service';
+import JwtService from './Jwt.service';
 // -|> implementando a interface IUserService , todos os metodos que ira ter interface IuserService tera que ter na classe
 // teste
 export default class UserService {
@@ -20,7 +21,13 @@ export default class UserService {
     console.log(credentials);
 
     const userInformation: IUser | null = await UserService.findByEmail(credentials.email);
+    // => validacao do email e senha
     if (!userInformation) throw new Errors(401, 'Incorrect email or password');
+
+    if (!EncryptService.validatePassword(credentials.password, userInformation.password)) {
+      throw new Errors(401, 'Incorrect email or password');
+    }
+
     // const { id, email, role, username } = userInformation;
     // -|> gerando o token apartir das informacoes do usuario
     const token = await JwtService.generateToken(userInformation);
