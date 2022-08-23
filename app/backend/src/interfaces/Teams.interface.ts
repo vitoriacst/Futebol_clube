799115a449eb3dfb06740848fefee41e9cbe7e62
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-23 20:44:01 -0300

    feat: :sparkles: creating and implementing the Team interface

---
+++ app/backend/src/interfaces/Teams.interface.ts
@@ -0,0 +1,11 @@
+import Teams from '../database/models/team.model';
+
+export interface ITeam{
+  id: number,
+  teamName: string,
+}
+
+export interface ITeamService {
+  getAll(): Promise<Teams[]>,
+  getById(id: number): Promise<Teams>
+}
