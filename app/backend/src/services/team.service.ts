acbcb47a47ee1454f6039a9bd18c80a2f9c5f4be
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-23 20:49:57 -0300

    refactor: :recycle: changing the interface from ITeam to ITeamService

--- app/backend/src/services/team.service.ts
+++ app/backend/src/services/team.service.ts
@@ -1,5 +1,10 @@
-import { ITeam } from '../interfaces/Teams.interface';
+import Teams from '../database/models/team.model';
+import { ITeamService } from '../interfaces/Teams.interface';
 
-export default class TeamService implements ITeam {
-
+export default class TeamService implements ITeamService {
+  // => forma que ele possa retornar todos os times corretamente
+  public getAll = async (): Promise<Teams[]> => {
+    const team : Teams[] = await Teams.findAll();
+    return team;
+  };
 }
