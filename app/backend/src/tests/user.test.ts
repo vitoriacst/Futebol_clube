f0e505ddcade7050f10d03d98d5652dc1dec8401
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-23 15:12:07 -0300

    build: :wrench: installing chai-http library and starting user tests

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -1,7 +1,17 @@
 // -|> iniciando os testes da rota Login 🚀
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import Encrypt from '../services/encrypt.service';
+import JwtService from '../services/Jwt.service';
+import { tokenMock } from './Mocks/mocksUser';
+import chaiHttp = require('chai-http');
+
+
+chai.use(chaiHttp)
 
 describe('Check if the login was successful', () => {
   beforeEach(()=>{
-    sinon.stub()
+    sinon.stub(Encrypt, 'validatePassword').returns(true);
+    sinon.stub(JwtService, 'generateToken').resolves(tokenMock.token)
   })
  })
