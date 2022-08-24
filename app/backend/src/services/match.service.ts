73c05863d2b10f627c832ef2de21920a636f4033
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-24 12:26:16 -0300

    feat: :sparkles: implementing the service layer's getAll (matches) method

---
+++ app/backend/src/services/match.service.ts
@@ -0,0 +1,48 @@
+import Match from '../database/models/matches.model';
+import Teams from '../database/models/team.model';
+
+// {
+//   [
+//     {
+//       "id": 1,
+//       "homeTeam": 16,
+//       "homeTeamGoals": 1,
+//       "awayTeam": 8,
+//       "awayTeamGoals": 1,
+//       "inProgress": false,
+//       "teamHome": {
+//         "teamName": "São Paulo"
+//       },
+//       "teamAway": {
+//         "teamName": "Grêmio"
+//       }
+//     },
+//     ...
+//     {
+//       "id": 41,
+//       "homeTeam": 16,
+//       "homeTeamGoals": 2,
+//       "awayTeam": 9,
+//       "awayTeamGoals": 0,
+//       "inProgress": true,
+//       "teamHome": {
+//         "teamName": "São Paulo"
+//       },
+//       "teamAway": {
+//         "teamName": "Internacional"
+//       }
+//     }
+//   ]
+// }
+
+export default class MatchService {
+  static getAll = async () => {
+    const matches = await Match.findAll({
+      include: [
+        { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+        { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+    return matches;
+  };
+}
