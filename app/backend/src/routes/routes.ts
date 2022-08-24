8b9c57c86933d22a0e1bfcbb77f2712c9a644c5c
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-24 13:38:26 -0300

    feat: :sparkles: creating the matches route

--- app/backend/src/routes/routes.ts
+++ app/backend/src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import MatchController from '../controllers/matches.controller';
 import TeamController from '../controllers/teamController';
 import UserController from '../controllers/userController';
 
@@ -12,5 +13,10 @@ application.get('/login/validate', (req:Request, res: Response) =>
 application.get('/teams', (req:Request, res: Response) => TeamController.getAll(req, res));
 
 application.get('/teams/:id', (req:Request, res: Response) => TeamController.getById(req, res));
+
+application.get(
+  '/matches',
+  (req:Request, res: Response) => MatchController.getAll(req, res),
+);
 
 export default application;
