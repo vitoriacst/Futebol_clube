8fe7dde0a355a94d032d6ea7c09221d82610efeb
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-24 13:35:51 -0300

    feat: :sparkles: implementing getAll method in controller layer

---
+++ app/backend/src/controllers/matches.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import MatchService from '../services/match.service';
+
+export default class MatchController {
+  static async getAll(request: Request, response: Response) {
+    const { inProgress } = request.query;
+    let result = [];
+    if (typeof inProgress === 'string') {
+      result = await MatchService.getAllProgress(
+        JSON.parse(inProgress),
+      );
+    } else {
+      result = await MatchService.getAll();
+    }
+    response.status(200).json(result);
+  }
+}
