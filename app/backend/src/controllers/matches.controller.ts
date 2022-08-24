9c39758e8d0545939db3df5ce1c627a1f79b0dad
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-24 12:35:18 -0300

    refactor: :recycle: refactoring the MatchesController class

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -1,5 +1,8 @@
-export default class Matche {
-  static getAll(_request:Request, response: Response) {
+// import MatchService from '../services/match.service';
 
-  }
-}
+// export default class Matche {
+//   static getAll(_request:Request, response: Response) {
+//     await MatchService.getAll();
+//     const result = await MatchService.getAll();
+//   }
+// }
