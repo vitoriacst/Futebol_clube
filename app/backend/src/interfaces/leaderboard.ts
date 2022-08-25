109073345dd73fdef00c4bb66e3387581e51f232
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-25 17:41:51 -0300

    feat: :construction: building the IScore interface

--- app/backend/src/interfaces/leaderboard.ts
+++ app/backend/src/interfaces/leaderboard.ts
@@ -9,3 +9,16 @@ export interface IGoals {
   goalsFavor: number;
   goalsOwn: number
 }
+
+export interface IScore{
+  name:string,
+  points: number,
+  games: number,
+  losses: number,
+  victories: number,
+  goalsLosses:number,
+  goalsFavor:number,
+  goalsOwn: number,
+  goalsTotal: number,
+  efficiency: string
+}
