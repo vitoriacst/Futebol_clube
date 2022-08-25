e6cc3ae1b04a747b677021216a11d98b0fd92c27
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-24 22:00:05 -0300

    refactor: :ambulance: performing the commit return

--- app/backend/src/services/Jwt.service.ts
+++ app/backend/src/services/Jwt.service.ts
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 import { JwtPayload, sign, verify } from 'jsonwebtoken';
 import { IJwtPayload } from '../interfaces/ILogin';
-import Errors from '../middlewares/Errors';
 
 dotenv.config();
 
@@ -14,12 +13,7 @@ export default class JwtService {
   }
 
   static validateToken = async (token: string) => {
-    try {
-      const { data: { role } } = verify(token, secret) as IJwtPayload;
-      if (!role) throw new Errors(401, 'Token must be a valid token');
-      return role;
-    } catch (error) {
-      throw new Errors(401, 'Token must be a valid token');
-    }
+    const role = verify(token, secret) as IJwtPayload;
+    return role.role;
   };
 }
