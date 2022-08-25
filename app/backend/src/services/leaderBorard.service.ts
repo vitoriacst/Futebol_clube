215ccaf16e91280e5b57afceb5419344409a1b0e
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-25 18:12:12 -0300

    feat: :sparkles: copying the HomeTeam matches method and adapted for the away Team

--- app/backend/src/services/leaderBorard.service.ts
+++ app/backend/src/services/leaderBorard.service.ts
@@ -15,4 +15,19 @@ export default class LeaderBoardService {
     const soccerMatches = HomeTeam.length;
     return { draws, victories, losses, soccerMatches };
   };
+
+  static matchesAwayTeam = async (teamId: number) => {
+    const matches = await MatchService.getAllProgress('false');
+    // => validando o id
+    const AwayTeam = matches.filter(({ awayTeam }) => awayTeam === teamId);
+    // => total de empates
+    const draws = AwayTeam.filter((element) => element.awayTeamGoals === element.homeTeamGoals);
+    // => total de vitorias
+    const victories = AwayTeam.filter((element) => element.awayTeamGoals > element.homeTeamGoals);
+    // total de partidas perdidas
+    const losses = AwayTeam.filter((element) => element.awayTeamGoals < element.homeTeamGoals);
+    // => total de partidas
+    const soccerMatches = AwayTeam.length;
+    return { draws, victories, losses, soccerMatches };
+  };
 }
