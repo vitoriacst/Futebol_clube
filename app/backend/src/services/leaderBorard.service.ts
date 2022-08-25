8c922214f9a0cc47823c9467484f4ec60c718f75
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-25 18:01:36 -0300

    feat: :construction: building logic with filters

--- app/backend/src/services/leaderBorard.service.ts
+++ app/backend/src/services/leaderBorard.service.ts
@@ -1,3 +1,13 @@
-export default class LeaderBoardService {
+import { IMatch } from '../interfaces/Match.interface';
+import MatchService from './match.service';
 
+export default class LeaderBoardService {
+  static matchesHomeTeam = async (teamId: number): Promise<IMatch> => {
+    const matches = await MatchService.getAllProgress('false');
+    // => validando o id
+    const HomeTeam = matches.filter(({ homeTeam }) => homeTeam === teamId);
+    // => total de empates
+    const draws = HomeTeam.filter((element) => element.homeTeamGoals === element.awayTeamGoals);
+    // => total de vitorias
+  };
 }
