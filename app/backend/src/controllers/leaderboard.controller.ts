14a496b939f2a2cf1c7575fa954dc62a69b08d96
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-25 20:20:06 -0300

    feat: :sparkles: implementing the away controller

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -4,7 +4,11 @@ import LeaderBoardService from '../services/leaderBoard.service';
 export default class LeaderBoardController {
   static async home(_request: Request, response: Response): Promise<void> {
     const result = await LeaderBoardService.chooseTeams('home');
-    const statusOk = 200;
-    response.status(statusOk).json(result);
+    response.status(200).json(result);
+  }
+
+  static async away(_request: Request, response: Response): Promise<void> {
+    const result = await LeaderBoardService.chooseTeams('away');
+    response.status(200).json(result);
   }
 }
