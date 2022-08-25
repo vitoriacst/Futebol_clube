601de86a33792609b974b2522be5295b576fe00b
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-25 20:19:17 -0300

    feat: :sparkles: implementing the home controller

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -1,5 +1,10 @@
-export default class LeaderBoardService {
-  static async home(request: Request, response: Response) {
-    const 
+import { Request, Response } from 'express';
+import LeaderBoardService from '../services/leaderBoard.service';
+
+export default class LeaderBoardController {
+  static async home(_request: Request, response: Response): Promise<void> {
+    const result = await LeaderBoardService.chooseTeams('home');
+    const statusOk = 200;
+    response.status(statusOk).json(result);
   }
 }
